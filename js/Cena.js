618d0f40f37259a75f02690d6d1aad3cc7e0be40
Brysah Vidal <brysahvidal.rodrigues@estudante.ufjf.br>
2022-01-23 20:43:28 -0300

    Cria mapa

--- js/Cena.js
+++ js/Cena.js
@@ -1,7 +1,7 @@
 export default class Cena {
     /* É RESPONSAVEL POR DESENHAR ELEMENTOS NA TELA EM UMA ANIMAÇÃO
     */ 
-    constructor(canvas) {
+    constructor(canvas,assets = null) {
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
         this.sprites = [];
@@ -9,24 +9,32 @@ export default class Cena {
         this.t0 = 0;
         this.dt = 0;
         this.idAnim = null;
+        this.assets = assets;
+         this.mapa = null;
     }
     desenhar() {
-        this.ctx.fillStyle = "grey";
+        this.ctx.fillStyle = "lightblue";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        for (let s = 0; s < this.sprites.length; s++) {
-            const sprite = this.sprites[s];
-            sprite.desenhar(this.ctx);
+        this.mapa?.desenhar(this.ctx);
+        if(this.assets.acabou()){
+            for(let i = 0; i < this.sprites.length; i++){
+                const sprite = this.sprites[i];
+                sprite.desenhar(this.ctx);
+            }
         }
+        this.ctx.fillStyle = "yellow";
+        this.ctx.fillText(this.assets?.progresso(), 10, 20);
     }
 
     adicionar(sprite) {
         this.sprites.push(sprite);
     }
     passo(dt)
-    {
-for (const sprite of this.sprites) {
-    sprite.passo(dt);
-}
+    { if(this.assets.acabou()){
+        for (const sprite of this.sprites) {
+            sprite.passo(dt);
+        }
+    }
 }
 quadro(t)
     {
@@ -79,5 +87,9 @@ quadro(t)
             }
         }
         this.aRemover = [];
+    }
+    configuraMapa(mapa){
+        this.mapa = mapa;
+        this.mapa.cena = this;
     }
 }
--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,7 @@
 import Cena from "./Cena.js"
 import Sprite from "./Sprite.js";
 import AssetManager from "./AssetManager.js";
+import Mapa from "./Mapa.js";
 
 const assets = new AssetManager();
 assets.carregaImagem("garota", "assets/garota.png");
@@ -10,7 +11,10 @@ assets.carregaAudio("moeda", "assets/coin.wav");
 
 const canvas = document.querySelector("canvas");
 const cena1 = new Cena(canvas, assets);
-
+canvas.width = 14 * 32;
+canvas.height = 10 * 32;
+const mapa1 = new Mapa(10, 14, 32);
+cena1.configuraMapa(mapa1);
 
 const pc = new Sprite({vx : 10});
 const en1  = new Sprite({x:140,w:30,color:"red"});
