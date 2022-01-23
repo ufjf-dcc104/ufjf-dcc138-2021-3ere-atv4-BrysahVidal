0aeab6d2dc7ea746d6fab812c88ade4a9abb0373
Brysah Vidal <brysahvidal.rodrigues@estudante.ufjf.br>
2022-01-23 19:00:54 -0300

    Adiciona Sprite com obj de parametro

--- js/Main.js
+++ js/Main.js
@@ -1,4 +1,10 @@
 import Cena from "./Cena.js"
+import Sprite from "./Sprite.js";
 const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 const cena1 = new Cena(canvas);
 cena1.desenhar();
+const en1 = new Sprite({ x: 140, y: 80, w: 30, h: undefined, color: 'red' });
+const pc = new Sprite();
+pc.desenhar(ctx);
+en1.desenhar(ctx); 
